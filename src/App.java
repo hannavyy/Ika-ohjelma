1027345025d059a873616fe002845ac2884af348
Hanna <hanna.vyy@gmail.com>
2026-09-05 08:12:01 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,63 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int ika = 17;   
+
+// Tulostusehdot   
+
+if (ika >= 0 && ika < 18)   
+
+   {   
+
+     System.out.println("Olet alaikäinen");   
+
+   }   
+
+   else   
+
+   {   
+
+     System.out.println("Olet aikuinen");   
+
+   }
+
+   if (ika > 0 && ika < 18)  
+  {  
+     System.out.println("Olet alaikäinen");  
+  } 
+  else if (ika >= 65)  
+  {  
+     System.out.println("Olet eläkeläinen");  
+  } 
+  else  
+  {  
+     System.out.println("Olet aikuinen"); 
+  }  
+
+  if (ika > 00 && ika < 18) { 
+
+System.out.println("Olet alaikäinen"); 
+
+if (ika >= 15) { 
+
+System.out.println("Saat ajaa mopoa"); 
+} 
+
+} 
+else if (ika >= 65) { 
+
+System.out.println("Olet eläkeläinen"); 
+
+} else { 
+
+System.out.println("Olet aikuinen"); 
+
+  } 
+
+  // Laajenna ohjelmaa siten, että ohjelma osaa kertoa 16-17-vuotiaille, että he voivat ajaa kevaria. 
+
+  if (ika > 15 && ika < 18) {
+    System.out.println ("Saat ajaa kevaria");
+  }
+} 
+    }
+
